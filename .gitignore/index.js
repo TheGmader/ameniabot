959ff3fffaaae93ea0add986bf746fbc99fee0e2
TheGmader <40711854+TheGmader@users.noreply.github.com>
2018-08-12 17:10:07 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -8,7 +8,7 @@ bot.on('ready', function() {
     console.log("Connecté - Pret à vous servir");
 });
 
-bot.login(process.ENV.TOKEN);
+bot.login(process.env.TOKEN);
 
 bot.on('message', message => {
     if (message.content === prefix + "help"){
